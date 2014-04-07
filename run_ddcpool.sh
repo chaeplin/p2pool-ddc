a920f53fbf7b2e1fe51f7b2bbf016a5ff6bb79db
chaeplin <chaeplin@gmail.com>
2014-04-08 06:29:11 +0900

    change seed node, change fee to 1%, change web-static

--- run_ddcpool.sh
+++ run_ddcpool.sh
@@ -8,7 +8,7 @@ if ps ax | grep -v grep | grep "$SERVICE" > /dev/null
 then
         echo "$SERVICE is already running!"
 else
-        screen -d -m -S P2P_DDC_DIFF python ./run_p2pool.py --net dokdocoin --give-author 0 --disable-upnp -f 0
+        screen -d -m -S P2P_DDC_DIFF python ./run_p2pool.py --net dokdocoin --give-author 0 --disable-upnp -f 1
 
 	wait
 fi
